215a5f7dd346a1004b9b02ccb566fe353a5fef04
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-12-26 21:57:42 +0200

    delete old java files

--- src/calculator/model/function/interpreter/CharacterValidator.java
+++
@@ -1,5 +0,0 @@
-package calculator.model.function.interpreter;
-
-public sealed interface CharacterValidator permits MathCharacterValidator {
-    boolean validateCharacter(char character);
-}
--- src/calculator/model/function/interpreter/MathCharacterValidator.java
+++
@@ -1,8 +0,0 @@
-package calculator.model.function.interpreter;
-
-public final class MathCharacterValidator implements CharacterValidator {
-    @Override
-    public boolean validateCharacter(char character) {
-        return Character.isLetter(character);
-    }
-}
