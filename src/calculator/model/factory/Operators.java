a8a99a93a700087dd6792ed96bb91bea13e08098
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-12-26 18:26:09 +0200

    Create Operators.java

---
+++ src/calculator/model/factory/Operators.java
@@ -0,0 +1,15 @@
+package calculator.model.function.factory;
+
+public final class Operators {
+    public static final char OPENING_BRACKET = '(';
+    public static final char CLOSING_BRACKET = ')';
+    public static final char PLUS = '+';
+    public static final char MINUS = '-';
+    public static final char MULTIPLY = '*';
+    public static final char DIVIDE = '/';
+    public static final char PERCENTAGE = '%';
+    public static final char POWER = '^';
+    public static final char OPPOSITE = '!';
+
+    private Operators() {}
+}
